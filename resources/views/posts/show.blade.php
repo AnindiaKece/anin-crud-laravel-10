d585631fde4b89f29c982b4db88a3e89ed6ed497
AnindiaKece <anindiaaulia4@gmail.com>
2024-02-23 13:53:16 +0700

    crud laravel 10

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Detail Data Post - SantriKoding.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <img src="{{ asset('storage/posts/'.$post->image) }}" class="w-50 rounded mx-auto d-block mb-3">
+                        <br>
+                        <label class="form-label badge bg-info text-wrap">Title</label>
+                        <h6>{{ $post->title }}</h6>
+                        <label class="form-label badge bg-info text-wrap">Content</label>
+                        <h6>{!! $post->content !!}</h6>
+
+                        <a href="{{route('posts.index')}}" class="btn btn-danger btn-sm">Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+@extends('layouts.app')
+
+@section('body')
+    <h1>Ini halaman depan</h1>
+@endsection
